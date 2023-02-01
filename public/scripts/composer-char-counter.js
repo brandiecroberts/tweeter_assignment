5536feb7988b564a9b86ecf990255e9b28acffbc
Brandie Roberts <brandiecroberts@gmail.com>
2023-02-01 09:52:36 -0800

    Add character counter to tweets and define a users tweet container

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  console.log("Hello World");
+
+  $(".new-tweet").keypress(function() {
+    const textLength = $(this).find("textarea").val().length;
+    $(this)
+      .find(".counter")
+      .val(140 - textLength);
+      
+    if (textLength > 140) {
+      $(this).find(".counter").css({ color: "red" });
+    }
+  });
+});
